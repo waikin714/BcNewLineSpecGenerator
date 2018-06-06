4f97d4813e2e5b36de676b52a46acda80bc22b8f
Weichien Su <souwaikin@gmail.com>
2018-06-07 00:54:04 +0800

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace bcNewLineSpec
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.NewEqTextBox = new System.Windows.Forms.TextBox();
+            this.AddEqButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.eqListBox = new System.Windows.Forms.ListBox();
+            this.DelEqButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.EventCheckListBox = new System.Windows.Forms.CheckedListBox();
+            this.AddEventButton = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SelectedListBox = new System.Windows.Forms.ListBox();
+            this.ProduceBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // NewEqTextBox
+            // 
+            this.NewEqTextBox.Location = new System.Drawing.Point(41, 23);
+            this.NewEqTextBox.Name = "NewEqTextBox";
+            this.NewEqTextBox.Size = new System.Drawing.Size(117, 22);
+            this.NewEqTextBox.TabIndex = 0;
+            // 
+            // AddEqButton
+            // 
+            this.AddEqButton.Location = new System.Drawing.Point(40, 51);
+            this.AddEqButton.Name = "AddEqButton";
+            this.AddEqButton.Size = new System.Drawing.Size(40, 22);
+            this.AddEqButton.TabIndex = 1;
+            this.AddEqButton.Text = "+";
+            this.AddEqButton.UseVisualStyleBackColor = true;
+            this.AddEqButton.Click += new System.EventHandler(this.AddEqButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(41, 83);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "EQ list:";
+            // 
+            // eqListBox
+            // 
+            this.eqListBox.FormattingEnabled = true;
+            this.eqListBox.ItemHeight = 12;
+            this.eqListBox.Location = new System.Drawing.Point(41, 109);
+            this.eqListBox.Name = "eqListBox";
+            this.eqListBox.Size = new System.Drawing.Size(138, 412);
+            this.eqListBox.TabIndex = 3;
+            // 
+            // DelEqButton
+            // 
+            this.DelEqButton.Location = new System.Drawing.Point(86, 51);
+            this.DelEqButton.Name = "DelEqButton";
+            this.DelEqButton.Size = new System.Drawing.Size(40, 22);
+            this.DelEqButton.TabIndex = 4;
+            this.DelEqButton.Text = "-";
+            this.DelEqButton.UseVisualStyleBackColor = true;
+            this.DelEqButton.Click += new System.EventHandler(this.DelEqButton_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(207, 28);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 12);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Event List";
+            // 
+            // EventCheckListBox
+            // 
+            this.EventCheckListBox.FormattingEnabled = true;
+            this.EventCheckListBox.HorizontalExtent = 10;
+            this.EventCheckListBox.HorizontalScrollbar = true;
+            this.EventCheckListBox.Location = new System.Drawing.Point(204, 53);
+            this.EventCheckListBox.Name = "EventCheckListBox";
+            this.EventCheckListBox.Size = new System.Drawing.Size(434, 463);
+            this.EventCheckListBox.TabIndex = 6;
+            this.EventCheckListBox.SelectedIndexChanged += new System.EventHandler(this.EventCheckListBox_SelectedIndexChanged);
+            // 
+            // AddEventButton
+            // 
+            this.AddEventButton.Location = new System.Drawing.Point(563, 28);
+            this.AddEventButton.Name = "AddEventButton";
+            this.AddEventButton.Size = new System.Drawing.Size(75, 23);
+            this.AddEventButton.TabIndex = 7;
+            this.AddEventButton.Text = ">>";
+            this.AddEventButton.UseVisualStyleBackColor = true;
+            this.AddEventButton.Click += new System.EventHandler(this.AddEventButton_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(663, 26);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(90, 12);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "All Selected Event";
+            // 
+            // SelectedListBox
+            // 
+            this.SelectedListBox.FormattingEnabled = true;
+            this.SelectedListBox.ItemHeight = 12;
+            this.SelectedListBox.Location = new System.Drawing.Point(665, 56);
+            this.SelectedListBox.Name = "SelectedListBox";
+            this.SelectedListBox.Size = new System.Drawing.Size(400, 460);
+            this.SelectedListBox.TabIndex = 9;
+            // 
+            // ProduceBtn
+            // 
+            this.ProduceBtn.Location = new System.Drawing.Point(990, 27);
+            this.ProduceBtn.Name = "ProduceBtn";
+            this.ProduceBtn.Size = new System.Drawing.Size(75, 23);
+            this.ProduceBtn.TabIndex = 10;
+            this.ProduceBtn.Text = "ProduceXls";
+            this.ProduceBtn.UseVisualStyleBackColor = true;
+            this.ProduceBtn.Click += new System.EventHandler(this.ProduceBtn_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1098, 552);
+            this.Controls.Add(this.ProduceBtn);
+            this.Controls.Add(this.SelectedListBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.AddEventButton);
+            this.Controls.Add(this.EventCheckListBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.DelEqButton);
+            this.Controls.Add(this.eqListBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.AddEqButton);
+            this.Controls.Add(this.NewEqTextBox);
+            this.Name = "MainForm";
+            this.Text = "MplcSpecGenerator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox NewEqTextBox;
+        private System.Windows.Forms.Button AddEqButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox eqListBox;
+        private System.Windows.Forms.Button DelEqButton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckedListBox EventCheckListBox;
+        private System.Windows.Forms.Button AddEventButton;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox SelectedListBox;
+        private System.Windows.Forms.Button ProduceBtn;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,278 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+using Microsoft.Office.Interop.Excel;
+using System.Diagnostics;
+
+namespace bcNewLineSpec
+{
+    public partial class MainForm : Form
+    {
+        string newEqTextBoxPlaceHolder = "Enter new EQ here...";
+        public static Excel.Application SrcExcelApp { get; set; } = null;
+        public static Workbook SrcWorkBook { get; set; } = null;
+        public static string ExeDir { get; set; } = null;
+
+        public class BcEvent
+        {
+            public int WorkSheetInd { get; set; } = -1;
+            public string EventName { get; set; } = null;
+            public Range EventRnage { get; set; } = null;
+
+            public BcEvent(int workSheetInd, string eventName, Range eventRange)
+            {
+                this.WorkSheetInd = workSheetInd;
+                this.EventName = eventName;
+                this.EventRnage = eventRange;
+            }
+            public int GetEventLength()
+            {
+                int firstRowInd = GetRangeRowInd(this.EventRnage, true);
+                int lastRowInd = GetRangeRowInd(this.EventRnage);
+                return lastRowInd - firstRowInd;
+            }
+            override
+            public string ToString()
+            {
+                return $"{this.EventName}";
+            }
+        }
+        public class EqBcEvent
+        {
+            public int EqIndex { get; set; } = -1;
+            public string EqName { get; set; } = null;
+            public BcEvent _BcEvent { get; set; } = null;
+
+            public EqBcEvent(int eqIndex, string eqName, BcEvent bcEvent)
+            {
+                this.EqIndex = eqIndex;
+                this.EqName = eqName;
+                this._BcEvent = bcEvent;
+            }
+            override
+            public string ToString()
+            {
+                return $"{this.EqIndex:D2} {this.EqName} [{this._BcEvent.ToString()} ({this._BcEvent.GetEventLength()}Word)]";
+            }
+        }
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            // newEqTextBox
+            NewEqTextBox.Text = newEqTextBoxPlaceHolder;
+            NewEqTextBox.GotFocus += RemoveText;
+            NewEqTextBox.LostFocus += AddText;
+
+            // eqListBox
+            eqListBox.Items.Add("Master PLC");
+
+            // open and load source excel file
+            MainForm.SrcExcelApp = new Excel.Application();
+            MainForm.SrcExcelApp.Visible = true;
+            string exePath = System.Reflection.Assembly.GetEntryAssembly().Location;
+            MainForm.ExeDir = System.IO.Path.GetDirectoryName(exePath);
+            MainForm.SrcWorkBook = MainForm.SrcExcelApp.Workbooks.Open(MainForm.ExeDir +
+                    "\\test.xls");
+                    //"\\InnoLux_T2_IOMapping_SummaryV2.xls");
+
+            // debug use ###
+            //Worksheet Ws = MainForm.SrcWorkBook.Worksheets[1];
+            //Ws.Range["A6:A10"].Rows.Group();
+            //Ws.Outline.SummaryRow = Microsoft.Office.Interop.Excel.XlSummaryRow.xlSummaryAbove;
+
+
+            List<BcEvent> allBcEventList = new List<BcEvent>();
+            ParseWorkSheet(MainForm.SrcWorkBook, allBcEventList);
+
+            
+
+            // eventList
+            foreach (var aBcEvent in allBcEventList)
+            {
+                EventCheckListBox.Items.Add(aBcEvent);
+            }
+            
+        }
+
+        
+        public static int GetRangeRowInd(Range aRange, Boolean reverse = false)
+        {
+            string rangeAddress = aRange.Address;
+            if (!reverse)
+            {
+                string lastRowIndStr = rangeAddress.Substring(rangeAddress.LastIndexOf("$") + 1);
+                return int.Parse(lastRowIndStr);
+            }
+
+            int startInd = rangeAddress.IndexOf("$", 1) + 1;
+            int endInd = rangeAddress.IndexOf(":");
+
+            string firstRowInd = rangeAddress.Substring(startInd, endInd - startInd);
+            return int.Parse(firstRowInd);
+        }
+
+        public void ParseWorkSheet(Workbook srcWorkBook, List<BcEvent> allBcEventList)
+        {
+            for (int workSheetInd = 1; workSheetInd <= srcWorkBook.Worksheets.Count; workSheetInd++)
+            {
+                Worksheet aWorkSheet = srcWorkBook.Worksheets[workSheetInd];
+                int maxRowInd = GetRangeRowInd(aWorkSheet.UsedRange);
+                for (int startRowInd = 2; startRowInd < maxRowInd; startRowInd++)
+                {
+                    string cellValue = aWorkSheet.Cells[startRowInd, 1].Value2?.ToString();
+                    if (cellValue == null) continue;
+
+                    int endRowInd = startRowInd + 1;
+                    do
+                    {
+                        string endCellValue = aWorkSheet.Cells[endRowInd, 2].Value2?.ToString();
+                        if (endCellValue == null) break;
+                        if (aWorkSheet.Cells[endRowInd, 2].MergeCells)
+                        {
+                            endRowInd = GetRangeRowInd(aWorkSheet.Cells[endRowInd, 2].MergeArea) + 1;
+                        }
+                        else
+                        {
+                            endRowInd++;
+                        }
+                    } while (true);
+
+                    Range eventRange = aWorkSheet.Range[aWorkSheet.Cells[startRowInd, 1], aWorkSheet.Cells[endRowInd - 1, 7]];
+                    allBcEventList.Add(new BcEvent(workSheetInd, cellValue, eventRange));
+                    startRowInd = endRowInd;
+                }
+            }
+        }
+        public void RemoveText(object sender, EventArgs e)
+        {
+            NewEqTextBox.Text = "";
+        }
+
+        public void AddText(object sender, EventArgs e)
+        {
+            if (String.IsNullOrWhiteSpace(NewEqTextBox.Text))
+                NewEqTextBox.Text = newEqTextBoxPlaceHolder;
+        }
+
+
+        private void AddEqButton_Click(object sender, EventArgs e)
+        {
+            eqListBox.Items.Add($"{NewEqTextBox.Text}");
+        }
+
+        private void DelEqButton_Click(object sender, EventArgs e)
+        {
+            if (eqListBox.SelectedIndex == -1)
+            {
+                MessageBox.Show("Please select EQ in eqList first", "Warning", MessageBoxButtons.OK);
+                return;
+            }
+            eqListBox.Items.RemoveAt(eqListBox.SelectedIndex);
+        }
+
+        public void BuildBorder(Range aRange)
+        {
+            Microsoft.Office.Interop.Excel.Borders border = aRange.Borders;
+            border[XlBordersIndex.xlEdgeLeft].LineStyle = Microsoft.Office.Interop.Excel.XlLineStyle.xlContinuous;
+            border[XlBordersIndex.xlEdgeTop].LineStyle = Microsoft.Office.Interop.Excel.XlLineStyle.xlContinuous;
+            border[XlBordersIndex.xlEdgeBottom].LineStyle = Microsoft.Office.Interop.Excel.XlLineStyle.xlContinuous;
+            border[XlBordersIndex.xlEdgeRight].LineStyle = Microsoft.Office.Interop.Excel.XlLineStyle.xlContinuous;
+        }
+
+
+        private void ProduceBtn_Click(object sender, EventArgs e)
+        {
+            // open destination excel file
+            var destworkBook = MainForm.SrcExcelApp.Workbooks.Add();
+            var destworkSheet = destworkBook.Worksheets.get_Item(1);
+
+            // copy 
+            int rowInd = 1;
+            foreach(EqBcEvent aEqBcEvent in SelectedListBox.Items)
+            {
+                // frist line
+                Range firstLineFirstCell = destworkSheet.Cells[rowInd, 1];
+                firstLineFirstCell.Value2 = aEqBcEvent.ToString(); // event Name
+                firstLineFirstCell.Font.Bold = true;
+                Range firstLine = destworkSheet.Range[destworkSheet.Cells[rowInd, 1], destworkSheet.Cells[rowInd, 8]];
+                firstLine.Interior.Color = System.Drawing.Color.MediumOrchid;
+                BuildBorder(firstLine);
+                
+                rowInd++;
+
+                // get original range position
+                Excel.Range from = aEqBcEvent._BcEvent.EventRnage;
+                int rangeLastRowInd = GetRangeRowInd(from);
+                int rangeStartRowInd = GetRangeRowInd(from, true);
+
+                // copy value and format
+                Worksheet oriWs = MainForm.SrcWorkBook.Worksheets[aEqBcEvent._BcEvent.WorkSheetInd];
+                // cut the first line, which is original event name
+                // first argument is relative to absolute cells(A1), second argument is decide the range size
+                Range cutFirstLineFrom = from.Range[oriWs.Cells[2, 2], 
+                                                    oriWs.Cells[2 + rangeLastRowInd - rangeStartRowInd - 1, 7]]; 
+                int toLastRowInd = rowInd + rangeLastRowInd - rangeStartRowInd - 1;
+                Excel.Range to = destworkSheet.Range[destworkSheet.Cells[rowInd, 3],
+                                                     destworkSheet.Cells[toLastRowInd, 9]];
+                cutFirstLineFrom.Copy();
+                to.PasteSpecial(XlPasteType.xlPasteAllUsingSourceTheme);
+                to.PasteSpecial(XlPasteType.xlPasteColumnWidths);
+                to.Rows.Group();
+
+                // add formula on first and second column of range
+                for(int formulaRowInd = rowInd; formulaRowInd <= toLastRowInd; formulaRowInd++)
+                {
+                    destworkSheet.Cells[formulaRowInd, 1].Formula = $"=RIGHT(B{formulaRowInd}, 5)";
+                    destworkSheet.Cells[formulaRowInd, 2].Formula = $"=IF(B{formulaRowInd - 1}<>\"\", \n" + 
+                            $"\"LW\" & RIGHT(\"0000\" & DEC2HEX(HEX2DEC(RIGHT({formulaRowInd - 1}, 4)) + 1), 4), \n" +
+                            $"\"LW\" & RIGHT(\"0000\" & DEC2HEX(HEX2DEC(RIGHT(B{formulaRowInd - 2}, 4)) + 1), 4))";
+                    BuildBorder(destworkSheet.Cells[formulaRowInd, 1]);
+                    BuildBorder(destworkSheet.Cells[formulaRowInd, 2]);
+                }
+
+                rowInd = toLastRowInd + 1;
+            }
+            destworkSheet.Cells[2, 1].Value = "L0000";
+            destworkSheet.Cells[2, 2].Value = "LW0000";
+            destworkBook.SaveAs( $"{MainForm.ExeDir}\\{DateTime.Now.ToString("yyyyMMddHHmm")}output.xls");
+
+            // close destination excel file
+            MainForm.SrcExcelApp.Application.DisplayAlerts = false;
+            MainForm.SrcExcelApp.Quit();
+
+            MessageBox.Show("Action finished", "Info", MessageBoxButtons.OK);
+            return;
+        }
+
+        private void EventCheckListBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void AddEventButton_Click(object sender, EventArgs e)
+        {
+            if (eqListBox.SelectedIndex == -1)
+            {
+                MessageBox.Show("Please select EQ in eqList first", "Warning", MessageBoxButtons.OK);
+                return;
+            }
+
+            foreach (var selectedItem in EventCheckListBox.CheckedItems)
+            {
+                EqBcEvent aEqBcEvent = new EqBcEvent(eqListBox.SelectedIndex + 1, eqListBox.SelectedItem.ToString(), (BcEvent)selectedItem);
+                SelectedListBox.Items.Add(aEqBcEvent);
+                SelectedListBox.Sorted = true;
+            }
+        }
+    }
+}
